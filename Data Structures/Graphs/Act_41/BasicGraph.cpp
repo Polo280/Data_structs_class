15e4d33b8efc9a59b47b8a2fac0a1a3f89c7917b
Polo280 <jorglezd28@gmail.com>
2023-11-05 21:20:17 -0600

    First implementation of graph

--- Data Structures/Graphs/Act_41/BasicGraph.cpp
+++ Data Structures/Graphs/Act_41/BasicGraph.cpp
@@ -1,55 +1,122 @@
 /* A basic implementation for a graph data structure
-Author = Jorge Gonzalez
 */
 #include <iostream>
 #include <cstdlib>
-
-struct Node
-{
-    int value;
-};
+#include <list>
+#include <vector>
+#include <queue>
 
 
 class Graph{
     private:
-        Node ** node_list;
+        std::list<int> *adj_list;
         int num_vertex, num_edges;
     public:
         // Constructor y destructor 
-        Graph(int vertex, int edges) : num_vertex(vertex), num_edges(edges){loadGraph(vertex, edges);};
+        Graph(int vertex) : num_vertex(vertex), adj_list(new std::list<int> [vertex]){};
         // Custom functions
-        void loadGraph(int, int);
-        void DFS();
+        void addEdge(int, int);
+        void DFS(int);
+        void BFS(int);
         void showGraph();
+        // Getters & setters
+        void setAdjList(std::list<int> adjList2){*adj_list = adjList2;};
 };
+
+void Graph::BFS(int startVertex) {
+    std::vector<bool> visited(num_vertex, false);
+    std::queue<int> queue;
+
+    visited[startVertex] = true;
+    queue.push(startVertex);
+
+    while (!queue.empty()) {
+        int currentVertex = queue.front();
+        queue.pop();
+
+        std::cout << "Visited: " << currentVertex << " ";
+        for (std::list<int>::iterator i = adj_list[currentVertex].begin(); i != adj_list[currentVertex].end(); ++i) {
+            int adjVertex = *i;
+            if (!visited[adjVertex]) {
+                visited[adjVertex] = true;
+                queue.push(adjVertex);
+            }
+        }
+    }
+    std::cout << std::endl;
+}
+
+void Graph::DFS(int startVertex) {
+    static std::vector<bool> visited(num_vertex, false);
+
+    visited[startVertex] = true;
+    std::cout << "Visited: " << startVertex << " ";
+
+    for (std::list<int>::iterator i = adj_list[startVertex].begin(); i != adj_list[startVertex].end(); ++i) {
+        int adjVertex = *i;
+        if (!visited[adjVertex]) {
+            DFS(adjVertex);
+        }
+    }
+}
+
+// Add an edge between two nodes
+void Graph::addEdge(int n1, int n2){
+    this->adj_list[n1].push_back(n2);
+}
+
 
 /**
 @brief Function to generate automatically random values for graph
 @param nodes number of nodes to store in the graph
 @param edges number of edges in the graph
 */
-void Graph::loadGraph(int nodes, int edges){
-    // Generate random nodes
-    *this->node_list = new Node;
+void loadGraph(int nodes, int edges, std::list<int> *&adjList){
+    std::vector<int*> node_vect;
+    // Add vertex at start of every list
+    for(int i=0;i < nodes; ++i){
+        int val = rand() % 50;
+        (adjList + i)->push_back(val);
+        node_vect.push_back(&(adjList + i)->front());
+    }
+    int current_edges = 0;
+    // Add edges randomly
     for(int i=0; i < nodes; ++i){
-        (*node_list + 1)->value = rand() % 100;  // Generate numbers 0-100
+        int connections = 0;
+        if(i >= nodes - 1){
+            connections = edges - current_edges;
+        }else{
+            connections = rand() % (edges - current_edges);
+        }
+        for(int k = 0; k < connections; ++k){
+            int* connection = node_vect[rand() % nodes];
+            while(connection == &(adjList + i)->front()){
+                connection = node_vect[rand() % nodes]; // To not connect it to itself
+            }
+            (adjList + i)->push_back(*connection);
+            ++current_edges;
+        }
     }
 }
 
-/**
-@brief Function to display adjacency list
-@param None
-*/
-void Graph::showGraph(){
-    Node* aux = *this->node_list;  // List starts here
-    for(int i=0; i < this->num_vertex; ++i){
-        std::cout << (aux + i)->value << "\n";
-        ++ aux;
-    }
-}
 
 int main(){
-    Graph g1(5, 7);
-    g1.showGraph();
+    const int num_vertex = 5; const int edges = 10;
+    std::list<int> *adjList = new std::list<int>[num_vertex];
+    loadGraph(num_vertex, edges, adjList); 
+    
+    std::cout << "Graph nodes are: ";
+    for(int i=0; i < num_vertex; ++i){
+        std::cout << (adjList + i)->front() << " - ";
+    }
+    std::cout << "\nGraph edges are: \n";
+    for(int i=0; i < num_vertex; ++i){
+        for(auto it : *(adjList +  i)){
+            std::cout << it << " - ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\n";
     return EXIT_SUCCESS;
 }
